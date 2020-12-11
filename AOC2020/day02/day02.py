353320a3321eb930f836ed59bf3dd995167ae3d1
AhmedGhanmi <Amustaphag22@gmail.com>
2020-12-11 12:39:54 +0100

    day02 python

--- AOC2020/day02/day02.py
+++ AOC2020/day02/day02.py
@@ -1,12 +1,4 @@
 # %%
-with open('input.txt') as txt: s=txt.read()
-c=0
-for line in s:
-    min,max,letter = int(line[0]), int(line[2]), line[4]
-    print(min,max,letter)
-    if (min<=len([x for x in line[7:] if x ==letter])<=max): c+=1
-print(c)
-# %%
 def parse(line):
     numbers=[]
     rest=[]
@@ -15,13 +7,31 @@ def parse(line):
     while(i<len(line) and len(line)>0):
         if line[i].isnumeric():
             number=""
-            while(line[i].isnumeric()):
+            while(i<len(line) and line[i].isnumeric()):
                 number+=line[i]
                 i+=1
             numbers.append(number)
-        if len(numbers) and letter=="":
+        if len(numbers)==2 and letter=="":
             letter=line[i+1]
             rest=line[i+4:]
             break
+        i+=1
+    numbers=[int(i) for i in numbers]
     return numbers, letter, rest
+# %%
+with open('input.txt') as txt: s=txt.readlines()
+s=[line.strip() for line in s]
+parsed = [parse(line) for line in s]
+count1 = len([1 for line in parsed if (line[0][0]<=len([i for i in line[2] if i==line[1]])<=line[0][1])])
+# %%
+print(count1)
+
+#%%
+def test(line):
+    try:
+        return (line[2][line[0][0]-1]==line[1]) != (line[2][line[0][1]-1]==line[1])
+    except:
+        return False
+count2 = len([1 for line in parsed if test(line)])
+
 # %%
